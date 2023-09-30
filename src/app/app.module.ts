5fed8c7543bcaa2382101d42e7c615fcf004e63f
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2023-09-30 16:03:40 +0300

    Add components for car inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BodyComponent } from './body/body.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatStepperModule} from '@angular/material/stepper';
+import { StepperComponent } from './body/stepper/stepper.component';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatStepperModule,
+    StepperComponent,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
