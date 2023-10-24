5be7b43aaa561a0dc8074649b27eb531312bb8c3
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2023-10-24 23:10:06 +0300

    Get value with http call and assign

--- src/app/body/stepper/stepper.component.ts
+++ src/app/body/stepper/stepper.component.ts
@@ -14,6 +14,8 @@ import { MatCardModule } from '@angular/material/card';
 import { CardComponent } from './card/card.component';
 import { CarsService } from 'src/app/services/cars.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Car } from 'src/app/interfaces/car';
 
 /**
  * @title Stepper horizontal
@@ -37,69 +39,63 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class StepperComponent implements OnInit {
   carEssentialInfo = this._formBuilder.group({
+    distance: ['0', Validators.required],
+    fuel_type: ['', Validators.required],
     make: ['', Validators.required],
     model: ['', Validators.required],
-    year: [new Date().getFullYear(), Validators.required],
-    distance: ['0', Validators.required],
-    color: ['', Validators.required],
     transmission: ['', Validators.required],
-    drivetrain: ['', Validators.required],
-    door: ['', Validators.required],
-    engine: ['', Validators.required],
+    year: [new Date().getFullYear(), Validators.required],
   });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
+  car: Car = {
+    make: '',
+    model: '',
+    year: 0,
+    distance: 0,
+    transmission: '',
+    fuel_type: '',
+  };
+
   isLinear = true;
 
   constructor(
     private _formBuilder: FormBuilder,
     carsService: CarsService,
-    private http: HttpClient
+    private http: HttpClient,
   ) {
-    this.cars = carsService.getCars();
-    console.log(this.cars);
+    // this.cars = carsService.getCars();
   }
 
   cars: Array<any> = [];
   ngOnInit() {
-    // const makeValue2 = this.carEssentialInfo.get('')
-    // const makeValue = this.carEssentialInfo?.get('make');
-    // if (makeValue) {
-    //   makeValue.valueChanges.subscribe(selectedMake => {
-    //     console.log('Selected make:', selectedMake);
-    //   });
-    // }
-    // console.log(this.cars);
-
-    // this.carEssentialInfo.statusChanges.subscribe(status => {
-    //   console.log('carEssentialInfo form status:', status);
-    // });
-
-    // this.carEssentialInfo.valueChanges.subscribe(value => {
-    //   console.log('carEssentialInfo form value:', value);
-    // });
+    this.carEssentialInfo.valueChanges.subscribe((value) => {
+      console.log('carEssentialInfo form value:', value);
+      this.car.make = value.make ?? '';
+      this.car.model = value.model ?? '';
+      this.car.year = Number(value.year) ?? 0;
+      this.car.distance = Number(value.distance) ?? 0;
+      this.car.transmission = value.transmission ?? '';
+      this.car.fuel_type = value.fuel_type ?? '';
+    });
     this.getCars();
   }
 
-  sendCar(cars: any) {
-    console.log(this.carEssentialInfo.value);
+  sendCar(car: Car) {
     const headers = new HttpHeaders({ dev: 'hbDev' });
     this.http
       .post(
         'https://car-estimator-4fe10-default-rtdb.europe-west1.firebasedatabase.app/cars.json',
-        cars,
+        car,
         { headers: headers }
       )
       .subscribe((res) => console.log(res));
   }
-  getCars() {
+  getCars(){
     this.http
-      .get(
+      .get<Car[]>(
         'https://car-estimator-4fe10-default-rtdb.europe-west1.firebasedatabase.app/cars.json'
-      )
-      .subscribe((res) => console.log(res));
-    console.log(this.carEssentialInfo.value.make);
-    console.log(this.carEssentialInfo.value.year || 'empty');
+      ).subscribe((res) => {
+        this.cars = Object.values(res);
+        this.cars.map((t) => console.log(t));
+      });
   }
 }
---
+++ src/app/interfaces/car.ts
@@ -0,0 +1,8 @@
+export interface Car {
+  distance: number;
+  fuel_type: string;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
